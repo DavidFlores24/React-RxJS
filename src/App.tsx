2f4828224a6972c1eabcca77ccf90ae290d1cdc1
David Flores <david.fgardens@gmail.com>
2023-02-21 19:00:02 +0000

    fix: reset current message on app click

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,17 @@ import {Sidebar} from "./components/Sidebar/Sidebar";
 import {ConversationDisplay} from "./components/ConversationDisplay/ConversationDisplay";
 import {NewMessage} from "./components/NewMessage/NewMessage";
 import './App.css';
+import {setCurrentMessage} from "./observables/currentMessage";
 
 function App() {
+    // Setting this here to reset the current message
+    // and allow users to send new message again
+    const handlePress = useCallback(() => {
+        setCurrentMessage(null);
+    }, []);
+
   return (
-    <div className="App">
+    <div className="App" onClick={handlePress}>
       <Sidebar/>
         <div className="chat-display">
             <ConversationDisplay />
